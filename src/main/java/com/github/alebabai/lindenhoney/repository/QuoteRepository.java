7fd7f41b2e799fae4e1ad06d445b5c9a741ed8e0
Alexander Babai <aliaksandr.babai@gmail.com>
2017-06-03 20:42:05 +0300

    feat(song): fing all quotes from song

--- src/main/java/com/github/alebabai/lindenhoney/repository/QuoteRepository.java
+++ src/main/java/com/github/alebabai/lindenhoney/repository/QuoteRepository.java
@@ -8,14 +8,19 @@ import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(exported = false)
 public interface QuoteRepository extends Repository<Quote, Integer> {
 
     @Query(value = "SELECT * FROM QUOTE ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Quote findRandomQuote();
 
-    @Query(value = "SELECT * FROM QUOTE q WHERE q.verse_id IN (SELECT s.id FROM SONG s WHERE s.id = :songId) ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM QUOTE q WHERE q.verse_id IN (SELECT v.id FROM VERSE v WHERE v.song_id = :songId) ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Quote findRandomQuoteFromSong(@Param("songId") Integer songId);
 
     Page<Quote> findQuotesByPhraseContainingIgnoreCase(@Param("phrase") String phrase, Pageable pageable);
+
+    @Query(value = "SELECT * FROM QUOTE q WHERE q.verse_id IN (SELECT v.id FROM VERSE v WHERE v.song_id = :songId)", nativeQuery = true)
+    List<Quote> findAllQuotesFromSong(@Param("songId") Integer songId);
 }
--- src/test/java/com/github/alebabai/lindenhoney/repository/QuoteRepositoryTest.java
+++ src/test/java/com/github/alebabai/lindenhoney/repository/QuoteRepositoryTest.java
@@ -2,6 +2,7 @@ package com.github.alebabai.lindenhoney.repository;
 
 import com.github.alebabai.lindenhoney.domain.Quote;
 import com.github.alebabai.lindenhoney.domain.Song;
+import com.github.alebabai.lindenhoney.domain.Verse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static com.github.alebabai.lindenhoney.util.EntityUtils.generateQuote;
 import static com.github.alebabai.lindenhoney.util.EntityUtils.generateSong;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.isIn;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest(showSql = false)
@@ -39,5 +42,17 @@ public class QuoteRepositoryTest {
 
         assertThat(randomQuote, notNullValue());
         assertThat(randomQuote, isIn(quotes));
+    }
+
+    @Test
+    public void findAllQuotesFromSong() {
+        final Quote quote1 = generateQuote();
+        final Quote quote2 = generateQuote();
+        final Verse verse = new Verse(asList(quote1, quote2));
+        final Song song = songRepository.save(generateSong()
+                .setVerses(singletonList(verse)));
+
+        final List<Quote> quotes = quoteRepository.findAllQuotesFromSong(song.getId());
+        assertThat(quotes, contains(quote1, quote2));
     }
 }
