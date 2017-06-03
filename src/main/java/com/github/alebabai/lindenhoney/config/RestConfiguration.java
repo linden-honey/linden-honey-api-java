dc5ac0cfbcd2a34fa640d88d42cfac8945ba6d55
Alexander Babai <aliaksandr.babai@gmail.com>
2017-06-03 18:54:06 +0300

    feat(rest): allow cors

---
+++ src/main/java/com/github/alebabai/lindenhoney/config/RestConfiguration.java
@@ -0,0 +1,17 @@
+package com.github.alebabai.lindenhoney.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RestConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getCorsRegistry()
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
