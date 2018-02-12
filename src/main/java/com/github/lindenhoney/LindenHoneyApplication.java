d7f0a6eb1e959f64a2dde6f324f09f89016bfb8a
Aliaksandr Babai <aliaksandr.babai@gmail.com>
2018-02-12 17:04:55 +0300

    feat(spring): enable WebFlux

--- src/main/java/com/github/lindenhoney/LindenHoneyApplication.java
+++ src/main/java/com/github/lindenhoney/LindenHoneyApplication.java
@@ -3,9 +3,11 @@ package com.github.lindenhoney;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
-@SpringBootApplication(scanBasePackages = "com.github.lindenhoney")
-@EnableReactiveMongoRepositories(basePackages = "com.github.lindenhoney.repository")
+@EnableWebFlux
+@EnableReactiveMongoRepositories(basePackages = {"com.github.lindenhoney.repository"})
+@SpringBootApplication(scanBasePackages = {"com.github.lindenhoney"})
 public class LindenHoneyApplication {
 
     public static void main(String[] args) {
