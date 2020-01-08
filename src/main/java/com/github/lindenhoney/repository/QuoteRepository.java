813a747dae4c260a5f9edfda65a27e5bc69c3ee9
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-08 15:47:46 +0300

    refactor(repository): clean up

--- src/main/java/com/github/lindenhoney/repository/QuoteRepository.java
+++ src/main/java/com/github/lindenhoney/repository/QuoteRepository.java
@@ -1,16 +1,14 @@
 package com.github.lindenhoney.repository;
 
-import com.github.lindenhoney.domain.Quote;
+import com.github.lindenhoney.entity.Quote;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(exported = false)
 public interface QuoteRepository extends Repository<Quote, Integer> {
 
     @Query(value = "SELECT * FROM QUOTE ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
--- src/main/java/com/github/lindenhoney/repository/VerseRepository.java
+++ src/main/java/com/github/lindenhoney/repository/VerseRepository.java
@@ -1,14 +1,12 @@
 package com.github.lindenhoney.repository;
 
-import com.github.lindenhoney.domain.Verse;
+import com.github.lindenhoney.entity.Verse;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(exported = false)
 public interface VerseRepository extends Repository<Verse, Integer> {
 
     @Query(value = "SELECT * FROM VERSE ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
