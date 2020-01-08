3c213e875334dc2d0c04d43b4847f943ea42ede8
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-08 17:32:25 +0300

    test: add integration test support

--- src/test/java/com/github/lindenhoney/ApplicationTest.java
+++ src/test/java/com/github/lindenhoney/ApplicationTest.java
@@ -1,13 +1,9 @@
 package com.github.lindenhoney;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-public class ApplicationTest {
+public class ApplicationTest extends AbstractIntegrationTest {
+
     @Test
     public void loadContextTest() {
 
